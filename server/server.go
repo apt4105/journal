4ec7e35541739bf1969006b10bde59e5dd0adfac
Diana Issatayeva <diana.issatayeva@gmail.com>
2019-10-26 02:56:12 -0700

    handles first part of API up to recognizing user folders

---
+++ server/server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"os"
+)
+
+func main() {
+	fmt.Println("vim-go")
+
+	fs := http.FileServer(http.Dir("/"))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if strings.HasPrefix(r.URL.Path, "/asset") {
+			s := strings.Split(r.URL.Path, "/")
+			fmt.Println(s[2])
+			//check if User dir exists
+			if _, err := os.Stat("asset/"+s[2]+"/"); os.IsNotExist(err) {
+				fmt.Fprint(w, "No such User, " + s[2]+".")
+				return
+			}
+
+			//if _, err := os.Stat("//")
+			//!os.IsNotExist(err) {
+			// path/to/whatever exists
+			//}
+			//http.StripPrefix("/asset", fs).ServeHTTP(w, r)
+			if _,  
+			return
+		}else {
+			fmt.Fprintf(w, "%s %s", r.Method, r.URL.Path)
+		}
+	})
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.ListenAndServe(":8080", nil)
+
+}
